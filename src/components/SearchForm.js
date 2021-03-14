3a977373751823b092e6e5a244011c1addf7634f
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-03-14 14:41:42 -0500

    completed sort function and revealed files in github:

---
+++ src/components/SearchForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function SearchForm(props) {
+  return (
+    
+      <div className="form-group">
+        <label htmlFor="search">Search:</label>
+        <input
+          onChange={props.handleInputChange}
+          value={props.search}
+          name="search"
+        
+          type="text"
+          className="form-control"
+          placeholder="Search for Employee"
+          id="search"
+        />
+        <button onClick={(e)=>props.toggleSort(e)} className="btn btn-primary mt-3">
+          Sort by first name {props.sort ? "descending" : "ascending"} order
+        </button>
+      </div>
+   
+  );
+}
+
+export default SearchForm;
